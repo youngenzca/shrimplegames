adb0c7e86bf50ae44ace8bd73cde28cc03748b0a
youngenzca <105184211+youngenzca@users.noreply.github.com>
2024-07-07 00:35:32 -0700

    Delete htdocs/DATABASE directory

--- htdocs/DATABASE/FAVORITE-GAMES-COMMENTS/table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE FavoriteGamesComments (
-	cid int(11) not null AUTO_INCREMENT PRIMARY KEY,
-    uid varchar(128) not null,
-    date varchar(128) not null,
-    message TEXT not null
-);
--- htdocs/DATABASE/SHRIMPLE-GAMES-COMMENTS/table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE ShrimpleGamesComments (
-	cid int(11) not null AUTO_INCREMENT PRIMARY KEY,
-    uid varchar(128) not null,
-    date varchar(128) not null,
-    message TEXT not null
-);
